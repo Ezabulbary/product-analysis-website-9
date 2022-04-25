9206993fe460ee6ae8eda304e4a5d46eb2dbc7bb
Ezabulbary <ezabul.bary@gmail.com>
2022-04-25 20:13:57 +0600

    header added

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import CustomLink from '../CustomLink/CustomLink';
 
 const Header = () => {
     return (
-        <div>
-            <h1>This is header</h1>
-        </div>
+        <nav className='h-[120px] md:h-[80px] bg-[#1C2B35] flex items-center justify-center'>
+            <div className='md:mx-10'>
+                <CustomLink className='text-white no-underline text-lg pl-6' to="/home">Home</CustomLink>
+                <CustomLink className='text-white no-underline text-lg pl-6' to="/reviews">Reviews</CustomLink>
+                <CustomLink className='text-white no-underline text-lg pl-6' to="/dashboard">Dashboard</CustomLink>
+                <CustomLink className='text-white no-underline text-lg pl-6' to="/blogs">Blogs</CustomLink>
+                <CustomLink className='text-white no-underline text-lg pl-6' to="/about">About</CustomLink>
+            </div>
+        </nav>
     );
 };
 
